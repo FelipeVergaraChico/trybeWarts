d7d1bca1c517e6f42142e13c8b61aac5838a7827
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-04-26 19:04:49 -0300

    requisito 5 feito

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const botao = document.querySelector('#button-password');
 
-
 botao.addEventListener('click', () => {
-    const email = document.querySelector('#email').value;
-    const senha = document.querySelector('#senha').value;
-    if (email === "tryber@teste.com" && senha === '123456') {
-       alert("Olá, Tryber!");
-    } else {
-        alert("Email ou senha inválidos.");
-    }
-})
-console.log(email);
+  const email = document.querySelector('#email').value;
+  const senha = document.querySelector('#senha').value;
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+const main = document.createElement('main');
+const forms = document.createElement('form');
+document.body.appendChild(main);
+main.appendChild(forms);
+
+forms.id = 'evaluation-form';
